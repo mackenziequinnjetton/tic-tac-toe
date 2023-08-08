6c2d2dbb7765227e6b2b3229dfe79033892a0ffe
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-08 07:58:50 -0600

    Move context and create test file

---
+++ src/contexts/GameContext/GameContext.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { GameContext } from './GameContext';
+
+const setup = () => {
+  render(
+    <GameContext.Provider value={{
+      boardData: {
+        boardRow1: [
+          { spaceId: 1, spaceValue: "." },
+          { spaceId: 2, spaceValue: "." },
+          { spaceId: 3, spaceValue: "." }
+        ],
+        boardRow2: [
+          { spaceId: 4, spaceValue: "." },
+          { spaceId: 5, spaceValue: "." },
+          { spaceId: 6, spaceValue: "." }
+        ],
+        boardRow3: [
+          { spaceId: 7, spaceValue: "." },
+          { spaceId: 8, spaceValue: "." },
+          { spaceId: 9, spaceValue: "." }
+        ]
+      },
+      setBoardData: jest.fn(),
+      currentToken: "X",
+      setCurrentToken: jest.fn(),
+      spaceNotOccupied: jest.fn(),
+      switchToken: jest.fn()
+    }}>
+      <div>GameContext</div>
+    </GameContext.Provider>
+  );
+}
+
+test('renders GameContext', () => {
+  setup();
+
+  const linkElement = screen.getByText(/GameContext/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('spaceNotOccupied returns expected true value', () => {
+  
+});
