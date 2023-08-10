79d817a75b84190b221ae53b183d7310d4edee2b
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-10 02:31:43 -0600

    Add more App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,3 +8,21 @@ test("renders App", () => {
   const linkElements = screen.getAllByText(/./i);
   expect(linkElements).toHaveLength(11);
 });
+
+test("renders GameHeader", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Player X, it's your turn!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Board", () => {
+  render(<App />);
+  const linkElements = screen.getAllByText(/\./i);
+  expect(linkElements).toHaveLength(9);
+});
+
+test("renders RestartButton", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Restart/i);
+  expect(linkElement).toBeInTheDocument();
+});
