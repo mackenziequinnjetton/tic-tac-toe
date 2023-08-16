4869139f978caab0ef3e96182eba6644a65acb7b
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-16 04:47:04 -0600

    Add MoveButton test

--- src/components/MoveButton/MoveButton.test.tsx
+++ src/components/MoveButton/MoveButton.test.tsx
@@ -43,3 +43,11 @@ test("doesn't render MoveButton with move=1 with empty board", () => {
   const linkElement = screen.queryByText(/1/i);
   expect(linkElement).toBeNull();
 });
+
+test("renders MoveButton with move=1 with non-empty board", () => {
+  setup(1, ["X", ".", ".", ".", ".", ".", ".", ".", "."], 2);
+  const linkElement = screen.getByText(/1/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
